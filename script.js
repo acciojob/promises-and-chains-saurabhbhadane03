fc31730c3b3c259808991aa6b7c38605b8dd2f53
Saurabh Rajendra Bhadane <102625571+saurabhbhadane03@users.noreply.github.com>
2023-03-23 23:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const age = document.querySelector(".age").value;
+const name = document.querySelector(".name").value;
+
+
+function myPromiseFnc(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			
+		}
+		,4000)
+	})
+}
+
+alert(age);
+alert(name);
